a3f94d5fda2cbe5c44fa7baea9d62bc061037be4
Mattias <ma10s@hotmail.com>
2019-11-11 12:50:08 +0100

    debugging memory leaks in fedml.py

--- cifarmodel.py
+++ cifarmodel.py
@@ -64,7 +64,7 @@ class KerasSequentialCifar(BaseLearner):
         avg_w = []
         for l in range(len(weights[0])):
             lay_l = np.array([w[l] for w in weights])
-            print("std layer ", l, ": ", np.std(lay_l))
+            print("std layer ", l, ": ", np.std(lay_l,0))
             weight_l_avg = np.mean(lay_l,0)
             avg_w.append(weight_l_avg)
 
