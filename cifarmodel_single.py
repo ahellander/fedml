bff6a8f137902a1a59b6dc9d3437d7f4b7f63425
Mattias <ma10s@hotmail.com>
2019-12-02 22:09:36 +0100

    added single training model (for accuracy comparison)

--- cifarmodel_single.py
+++ cifarmodel_single.py
@@ -12,7 +12,7 @@ import keras.backend as K
 
 import psutil
 from keras.datasets import cifar10
-
+learning_rate = 0.001
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
